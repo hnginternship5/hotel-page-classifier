7cd221d5d6a0d0046767f01437401fc2d379b80b
edeediong <36479528+edeediong@users.noreply.github.com>
2019-04-25 01:00:38 +0100

    Update

--- External_Internal.py
+++ External_Internal.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 24 12:31:45 2019
+Created on Wed Apr 25 00:15:26 2019
 
-@author: Jesse Amamgbu
+@author: Jesse Amamgbu,Kontrol
 """
 from urllib.request import urlopen
 from urllib.parse import urlparse
@@ -16,65 +16,76 @@ pages = set()
 random.seed(datetime.datetime.now())
 
 def getInternalLinks(bs, includeUrl):
-  includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme,urlparse(includeUrl).netloc)    
-  internalLinks = []    #Finds all links that begin with a "/"
-  for link in bs.find_all('a',href=re.compile('^(/|.*'+includeUrl+')')):
-    if link.attrs['href'] is not None:
-      if link.attrs['href'] not in internalLinks:
-        if(link.attrs['href'].startswith('/')):
-          internalLinks.append(includeUrl+link.attrs['href'])                
-        else:      
-          internalLinks.append(link.attrs['href'])
-  return internalLinks
+  try:
+    includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme,urlparse(includeUrl).netloc)    
+    internalLinks = []    #Finds all links that begin with a "/"
+    for link in bs.find_all('a',href=re.compile('^(/|.*'+includeUrl+')')):
+      if link.attrs['href'] is not None:
+        if link.attrs['href'] not in internalLinks:
+          if(link.attrs['href'].startswith('/')):
+            internalLinks.append(includeUrl+link.attrs['href'])                
+          else:      
+            internalLinks.append(link.attrs['href'])
+    return internalLinks
+  except:
+    print('HTTP error, link is not accessible')
+    return ''
 
 
 def getExternalLinks(bs,excludeUrl):
   externalLinks =[]
-  
-  for link in bs.find_all('a',href=re.compile('^(https|http|www)((?!'+excludeUrl+').)*$')):
-    if link.attrs['href'] is not None:
-      if link.attrs['href'] not in externalLinks:
-        externalLinks.append(link.attrs['href'])
-  return externalLinks
+  try:
+    for link in bs.find_all('a',href=re.compile('^(https|http|www)((?!'+excludeUrl+').)*$')):
+      if link.attrs['href'] is not None:
+        if link.attrs['href'] not in externalLinks:
+          externalLinks.append(link.attrs['href'])
+    return externalLinks
+  except:
+    print('HTTP error,link can not be accessed')
+    return ''
 
 
 #-------------------------------------------------------------------------------------------------#
 # Collects a list of all external URLs found on the site
 allExtLinks = set() 
 def getAllExternalLinks(siteUrl):
-  html = urlopen(siteUrl)
-  domain = '{}://{}'.format(urlparse(siteUrl).scheme,urlparse(siteUrl).netloc)
-  bs = BeautifulSoup(html, 'html.parser')
-  internalLinks = getInternalLinks(bs, domain)
-  externalLinks = getExternalLinks(bs, domain)
-  for link in externalLinks:
-    if link not in allExtLinks:
-      allExtLinks.add(link)
-      print(link)
-  for link in internalLinks:
-    if link not in allIntLinks:
-      allIntLinks.add(link)
-      getAllExternalLinks(link)
-
+  try:
+    html = urlopen(siteUrl)
+    domain = '{}://{}'.format(urlparse(siteUrl).scheme,urlparse(siteUrl).netloc)
+    bs = BeautifulSoup(html, 'html.parser')
+    internalLinks = getInternalLinks(bs, domain)
+    externalLinks = getExternalLinks(bs, domain)
+    for link in externalLinks:
+      if link not in allExtLinks:
+        allExtLinks.add(link)
+        print(link)
+    for link in internalLinks:
+      if link not in allIntLinks:
+        allIntLinks.add(link)
+        getAllExternalLinks(link)
+  except:
+    print('Error in getting external link')
 #------------------------------------------------------------------------------------------------#
 # Collects a list of all internal URLs found on the site
 
 allIntLinks = set()
 def getAllInternalLinks(siteUrl):
-  html = urlopen(siteUrl)
-  domain = '{}://{}'.format(urlparse(siteUrl).scheme,urlparse(siteUrl).netloc)
-  bs = BeautifulSoup(html, 'html.parser')
-  internalLinks = getInternalLinks(bs, domain)
+  try:
+    html = urlopen(siteUrl)
+    domain = '{}://{}'.format(urlparse(siteUrl).scheme,urlparse(siteUrl).netloc)
+    bs = BeautifulSoup(html, 'html.parser')
+    internalLinks = getInternalLinks(bs, domain)
   
-  for link in internalLinks:
-    if link not in allExtLinks:
-      allIntLinks.add(link)
-      print(link)
-      
+    for link in internalLinks:
+      if link not in allExtLinks:
+        allIntLinks.add(link)
+        print(link)
+  except:
+    print('Error in getting internal link')    
 
 #-----------------------------------------------------------------------------------------------#
 #In order to test script:Use code below
 
-# getAllExternalLinks('https://theblowfishgroup.com/hotel/')
-#getAllInternalLinks('https://theblowfishgroup.com/hotel/')
+#getAllExternalLinks(desiredlink)
+#getAllInternalLinks(desiredlink)
 
