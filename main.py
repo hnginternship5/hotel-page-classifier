f5687940d16d994ae2a71c71492173cb9ae303d5
haksoat <shopejuh@gmail.com>
2019-04-29 19:52:50 +0100

    Removed irrelevant code

--- main.py
+++ main.py
@@ -24,25 +24,4 @@ tags = []
 
 for name, website in zip(hotel_names, hotel_websites):
 	External_Internal.getAllInternalLinks(website)
-	internal_links = csv_read.column_to_list("internal.csv", "Urls")
-	for internal_link in internal_links:
-		if internal_link is None:
-			continue
-		tag_search = tags_search.TagSearch(internal_link)
-		tag_search.fetch_page()
-		tag_search.fetch_domain_name()
-		tag_search.find_title_tags()
-		tag_search.find_h1_tags()
-		tag_search.find_h2_tags()
-		tag_search.find_internal_a_tags()
-		tag_search.find_title_tags()
-		new_tags = tag_search.get_dict()
- 
-		tags.append(new_tags)
-
-	file_path = os.path.join(websites_directory, "{}.json".format(name))
-
-	with open(file_path, "w") as file:
-		json.dump(tags, file, indent=2)
-		
-	tags = []
+	
