5a8f471a33954b9ca0b17db54b4a66c53a8bd9ce
haksoat <shopejuh@gmail.com>
2019-04-30 16:18:19 +0100

    CLI feature

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import csv
-import csv_read
-import External_Internal
+import argparse
+import sys
+from External_Internal import getAllInternalLinks as gAI
+from textpreprocessing import *
 
-#<<<<<<< HEAD
-hotel_websites = csv_read.column_to_list("ibnahmadbello.csv", "valid_website")
-#=======
-#hotel_websites = csv_read.column_to_list("1000sites.csv", "valid_website")
-#>>>>>>> 145252e91af0740979563e1bda939b07ed15e189
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-u',
+        '--url',
+        help='This should be the url of the web page to be processed'
+    )
+    
+    if parser.parse_args().url is None:
+    	parser.error("You need to pass in a value for --url")
+    return parser.parse_args()
 
-for website in hotel_websites:
-	internal_links = External_Internal.getAllInternalLinks(website)
-	External_Internal.save_csv(internal_links)
-	
+if __name__ == "__main__":
+	args = parse_args()
+	url = args.url
+
+	def predictor(url):
+	  scraped_url = gAI(url)
+	  for link in scraped_url:
+	  	#dt is a variable from the textpreprocessing module
+	    y_pred= dt.predict(count_vectorizer.fit_transform([link]))
+	    print(y_pred, link)
+	    
+	predictor(url)
