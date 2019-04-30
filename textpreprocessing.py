04e6cc90603f1377d73c14741e2146c423415a5b
edeediong <36479528+edeediong@users.noreply.github.com>
2019-04-30 22:19:26 +0800

    Update textpreprocessing.py

--- textpreprocessing.py
+++ textpreprocessing.py
@@ -22,10 +22,9 @@ df['Tokens'] = df['Url'].apply(lambda x: extract_words(x))
 from sklearn.model_selection import train_test_split
 x= df['Url']
 y=df['label']
-x_train, x_test, y_train, y_test= train_test_split(x,y, test_size=0.2)
 from sklearn.feature_extraction.text import CountVectorizer
 
-count_vectorizer = CountVectorizer(analyzer=extract_words,stop_words=ignore_words,vocabulary=['about', 'homepage',"contact",'gallery', "blog"])
+count_vectorizer = CountVectorizer(analyzer=extract_words,stop_words=ignore_words,vocabulary=['about',"contact",'gallery', "blog"])
 count_train = count_vectorizer.fit_transform(x)
 feature_names = count_vectorizer.get_feature_names()
 print(feature_names)
@@ -33,9 +32,14 @@ X_vect = pd.DataFrame(count_train.toarray())
 from sklearn.tree import DecisionTreeClassifier
 dt= DecisionTreeClassifier()
 dt.fit(X_vect,y)
-# y_pred=dt.predict(count_vectorizer.fit_transform(['https://hotels.ng/blog-zambiza']))
-# print(y_pred)
-url=input()
-y_pred=dt.predict(count_vectorizer.fit_transform([url]))
-print (y_pred)
+
+from External_Internal import getAllInternalLinks as GLA
+def predictor():
+  url=input()
+  scraped_url = GLA(url)
+  for link in scraped_url:
+    y_pred= dt.predict(count_vectorizer.fit_transform([link]))
+    print(y_pred,url)
+    
+predictor()
 
